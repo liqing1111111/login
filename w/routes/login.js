2e1544a738dcb517c5aeaa4b12a819ef09da11e6
dllo <1316049848@qq.com>
2017-04-24 20:31:24 +0800

    aa

---
+++ w/routes/login.js
@@ -0,0 +1,15 @@
+/**
+ * Created by dllo on 2017/4/24.
+ */
+//创建路由的步骤
+//    引入express
+var express = require('express');
+//创建路由
+// 通过express创建路由
+var router = express.Router();
+//3.路由访问,回调函数
+router.get('/',function (req,res) {
+    res.render('login');
+});
+//模块导出 路由
+module.exports = router;
